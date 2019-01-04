88ba1575fd3f1f4f3819722946156e834b0cab1a
shiyy123 <2522984072@qq.com>
2019-01-04 19:09:55 +0800

    swragger monitor

---
+++ src/main/java/nju/edu/travel/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package nju.edu.travel.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Cary on 19-1-4
+ * Email: yangyangshi@smail.nju.edu.cn
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Value("${swagger.enable}")
+    private boolean enableSwagger;
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(enableSwagger)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("nju.edu.travel.web.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Travel API Doc")
+                .contact(new Contact("cary", "http://xueshu.baidu.com/scholarID/CN-BOFJ2HBK", "yangyangshi@smail.nju.edu.cn"))
+                .version("1.0")
+                .build();
+    }
+}
